e1062908d95d2a2e861a9c3b0fba4a2e286f33df
vsmottin <vsmottin@outlook.com>
2026-07-03 10:04:06 -0300

    feat: add commands & utf-8 conversion

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,61 @@
 #include "cores.h"
 #include "utils.hpp"
 #include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 using namespace std;
 
-int main()
-{
+int main(){
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
+
     string fileName;
     Ext4::FileSystemManager fs;
     bool img_chosen = false;
-    while (!img_chosen)
-    {
+    while (!img_chosen){
         cout << amarelo << "ext4shell " << reset;
         cin >> fileName;
         img_chosen = fs.setImage(trim(fileName));
     }
+
     fileName = trim(fileName);
 
     string comand;
-    while (true)
-    {
+    while (true){
         cout << amarelo << "ext4shell:" << reset azul << "[" << fileName << "/]" << reset << "$ ";
         cin >> comand;
         comand = trim(comand);
-        if (comand == "info")
-        {
+
+        if (comand == "info"){
             fs.info();
-        }
-        else if (comand == "ls")
-        {
+
+        } else if (comand == "attr"){
+            string alvo;
+            cin >> alvo;
+            fs.attr(trim(alvo));
+
+        } else if (comand == "ls"){
             fs.ls();
-        }
-        else if (comand == "exit")
-        {
+        
+        } else if (comand == "testi"){
+            uint32_t inode;
+            cin >> inode;
+            fs.testi(inode);
+        
+        } else if (comand == "testb"){
+            uint32_t block;
+            cin >> block;
+            fs.testb(block);
+
+        } else if (comand == "exit"){
             break;
-        }
-        else
-        {
-            cout << vermelho << "Comando invalido" << reset << endl;
+        
+        } else {
+            cout << vermelho << "Comando inválido" << reset << endl;
         }
     }
 
